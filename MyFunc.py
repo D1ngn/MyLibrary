b7f464e6ecc01b412eadd62a1d097ef723992a02
D1ngn <naganod1@gmail.com>
2020-08-05 17:34:23 +0900

    線形回帰追加

--- MyFunc.py
+++ MyFunc.py
@@ -10,6 +10,16 @@ from PIL import Image
 import librosa
 import soundfile as sf
 
+# 機械学習用
+# モデル
+from sklearn.linear_model import LinearRegression
+# 学習用
+from sklearn.model_selection import train_test_split
+#評価用
+from sklearn.metrics import accuracy_score
+
+
+
 
 """
 画像処理
@@ -22,14 +32,24 @@ import soundfile as sf
 音声処理
 """
 #　音声データをロードし、指定された秒数とサンプリングレートでリサンプル
-def load_audio_file(file_path, length, sampling_rate=16000):
-    data, sr = librosa.load(file_path, sr=sampling_rate)
+def load_audio_file(file_path, length, num_channels, sampling_rate=16000):
+    data, sr = sf.read(file_path)
     # データが設定値よりも大きい場合は大きさを超えた分をカットする
     # データが設定値よりも小さい場合はデータの後ろを0でパディングする
-    if len(data) > sampling_rate*length:
-        data = data[:sampling_rate*length]
+    # 1ch(モノラル)の場合
+    if num_channels == 1:
+        if len(data) > sampling_rate*length:
+            data = data[:sampling_rate*length]
+        else:
+            data = np.pad(data, (0, max(0, sampling_rate*length - len(data))), "constant")
+    # マルチチャンネルの場合
+    elif num_channels > 1:
+        if data.shape[0] > sampling_rate*length:
+            data = data[:sampling_rate*length, :]
+        else:
+            data = np.pad(data, [(0, max(0, sampling_rate*length-data.shape[0])), (0, 0)], "constant")
     else:
-        data = np.pad(data, (0, max(0, sampling_rate*length - len(data))), "constant")
+        print("please designate correct num_channels")
     return data
 
 # 音声データを指定したサンプリングレートで保存
@@ -51,6 +71,41 @@ def wave_to_spec(data, n_fft, hop_length, win_length):
     phase = np.exp(1.j * np.angle(spec)) # 位相スペクトログラムを取得(フェーザ表示)
     # mel_spec = librosa.feature.melspectrogram(data, sr=sr, n_mels=128) # メルスペクトログラムを用いる場合はこっちを使う
     return mag, phase
+
+
+"""
+機械学習手法
+"""
+# 線形回帰
+def linear_regression(X_train, X_test, y_train, y_test):
+    """
+    args:
+        X_train, X_test: Features matrix (shape: [n_samples, n_features])
+        y_train, y_test: Target array (shape: [n_samples,])
+
+    return:
+        y_model: predicted result of model
+    """
+    model = LinearRegression(fit_intercept=True)
+    model.fit(X_train, y_train) # モデルの学習
+    y_model = model.predict(X_test) #　モデルの推論
+    plt.scatter(X_train, y_train) # 学習データをプロット
+    plt.plot(X_test, y_model) # テスト結果を直線でつないでプロット
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+    return y_model
+
+
+# 分類
+
+
+
+# 次元削減
+
+
+
 
 """
 ファイル管理用
@@ -90,10 +145,20 @@ if __name__ == "__main__":
     # 図のスタイルを変更
     sns.set()
 
-    x = np.arange(-5, 5, 0.1)
-    mean = 0
-    std = 1
+    # ガウス分布用
+    # x = np.arange(-5, 5, 0.1)
+    # mean = 0
+    # std = 1
+    #
+    # gaussian(x, mean, std)
 
-    gaussian(x, mean, std)
-
+    #　三角関数用
+    # x = np.arange(-5, 5, 0.1)
     # sin(x)
+
+    # 線形回帰用
+    x = 10 * np.random.rand(50)
+    X = x[:, np.newaxis] # shape: [n_samples,] → [n_samples, n_features]の形式へ
+    y = 3 * x + np.random.randn(50)
+    X1, X2, y1, y2 = train_test_split(X, y, random_state=0, test_size=0.33) # データをtrain用とtest用に2:1で分ける
+    output = linear_regression(X1, X2, y1, y2)
